8888fa4dfd691877d4dd962df41034004da54145
Matthew Hibbs <mhibbs@trinity.edu>
2023-02-15 23:03:15 -0600

    Updated Third Person to use Cinemachine in new scene

--- Assets/Scripts/ThirdPerson/PlayerControllerThirdPerson.cs
+++ Assets/Scripts/ThirdPerson/PlayerControllerThirdPerson.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 
 public class PlayerControllerThirdPerson : MonoBehaviour
 {
-    public float forwardSpeed = 5000f;
-    public float strafeSpeed = 2000f;
+    public float speed = 5000f;
+    public float jumpPower = 1000f;
     public float turnSpeed = 1000f;
     public LayerMask groundMask;
     public float airDamping = 0.1f;
     
+    public Transform cam = null;
+    
     private Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.15f;
     private Rigidbody rb;
 
+    private float angleVelocity;
     void Start()
     {
         groundCheck = transform.Find("GroundCheck");
@@ -24,28 +27,45 @@ public class PlayerControllerThirdPerson : MonoBehaviour
 
     private void Update()
     {
-        float turnAmount =  turnSpeed * Time.deltaTime * Input.GetAxis("Mouse X");
-        transform.Rotate(transform.up, turnAmount);
+        float horiz = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
         
+        Vector3 moveForce = Vector3.zero;
+
+        if (cam == null)
+        {
+            moveForce = vert * transform.forward + horiz * transform.right;
+            float turnAmount =  turnSpeed * Time.deltaTime * Input.GetAxis("Mouse X");
+            transform.Rotate(transform.up, turnAmount);
+        }
+        else 
+        {
+            Vector3 inputDirection = new Vector3(horiz, 0f, vert);
+            if (inputDirection.magnitude > 0.1f)
+            {
+                float inputAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+                //Update inputAngle to move relative to camera angle
+                inputAngle += cam.eulerAngles.y;
+
+                float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, inputAngle, ref angleVelocity, 0.1f);
+                transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+
+                moveForce = Quaternion.Euler(0f, inputAngle, 0f) * Vector3.forward;
+            }
+        }
+
         bool grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        //Debug.Log(grounded);
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 moveForce =  forwardSpeed * vertical * Time.deltaTime * transform.forward + 
-                             strafeSpeed * horizontal * Time.deltaTime * transform.right;        
         if (grounded)
         {
-
-            rb.AddForce(moveForce);
-            
+            rb.AddForce(speed * Time.deltaTime * moveForce);
             if (Input.GetButtonDown("Jump"))
             {
-                rb.AddForce(Vector3.up * 1000f);    
+                rb.AddForce(Vector3.up * jumpPower);    
             }
         }
         else // in air
         {
-            rb.AddForce(moveForce * airDamping);
+            rb.AddForce(airDamping * speed * Time.deltaTime * moveForce);
         }
         
     }
--- Assets/Scripts/ThirdPerson/TPCamera.cs
+++ Assets/Scripts/ThirdPerson/TPCamera.cs
@@ -22,6 +22,7 @@ public class TPCamera : MonoBehaviour
     //CameraMove_Track();
     //CameraMove_Follow();
     CameraMove_Follow(true);
+    //CameraMove_IndependentRotation();
   }
 
   void CameraMove_Track()
@@ -43,6 +44,35 @@ public class TPCamera : MonoBehaviour
     {
       transform.rotation = Quaternion.RotateTowards(transform.rotation, initialRotation, damping * Time.deltaTime);
     }
+
+    Vector3 desiredPosition = target.position + transform.rotation * positionOffset;
+    Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+    transform.position = position;
+  }
+
+  private float angleX = 0f;
+  public float rotationSpeed = 5f;
+  void CameraMove_IndependentRotation()
+  {
+    float mx, my;
+    mx = Input.GetAxis("Mouse X");
+    my = Input.GetAxis("Mouse Y");
+
+    // We apply the initial rotation to the camera.
+    Quaternion initialRotation = Quaternion.Euler(angleOffset);
+
+    Vector3 eu = transform.rotation.eulerAngles;
+
+    angleX -= my * rotationSpeed;
+
+    // We clamp the angle along the X axis to be between 
+    // the min and max pitch.
+    angleX = Mathf.Clamp(angleX, -30f,30f);
+
+    eu.y += mx * rotationSpeed;
+    Quaternion newRot = Quaternion.Euler(angleX, eu.y, 0.0f) * initialRotation;
+
+    transform.rotation = newRot;
 
     Vector3 desiredPosition = target.position + transform.rotation * positionOffset;
     Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
